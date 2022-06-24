eb4095cc76b5d450c472afd07fdf9dcd3239a08e
victorinoLavida <victorinolavida@ciencias.unam.mx>
2022-06-23 22:24:53 -0500

    agregado popup

--- src/index.js
+++ src/index.js
@@ -19,150 +19,34 @@ const $buttons = document.querySelector("#buttons")
 const $buttonsFunctions = document.querySelector('.container-temporizador');
 const $buttonPopup = document.querySelector('.btn-popup')
 
-//version angitua xd
-const initTemporizador = () => {
-  let start = false;
-  let temporizador;
-  let horas = +$horas.textContent
-  let minutos = +$minutos.textContent
-  let segundos = +$segundos.textContent
-
-
-
-  $buttonsFunctions.addEventListener('click', ({ target }) => {
-    // if (target == $btnHorasSumar) {
-    //   $horas.innerText = horas++
-    // }
-    switch (target) {
-      case $btnHorasSumar:
-        horas = horas + 1
-        $horas.innerText = horas
-        console.log(horas)
-        return
-      case $btnHorasRestar:
-        horas = Math.max(horas - 1, 0)
-        return $horas.innerText = horas
-      case $btnMinutosSumar:
-        minutos = minutos + 1
-        if (minutos == 60) {
-          horas = horas + 1
-          $horas.innerText = horas
-          minutos = 0
-        }
-        return $minutos.innerText = minutos
-
-      case $btnMinutosRestar:
-        minutos = Math.max(minutos - 1, 0);
-        $minutos.innerHTML = minutos
-        return;
-      case $btnMegundosSumar:
-        segundos = segundos + 1;
-        if (segundos === 60) {
-          minutos = minutos + 1;
-          $minutos.innerHTML = minutos;
-          segundos = 0
-        }
-        return $segundos.innerText = segundos
-
-      case $btnMegundosRestar:
-        segundos = Math.max(segundos - 1, 0);
-        return $segundos.innerText = segundos;
-
-      default:
-
-        return null
-    }
-  })
-
-  $buttons.addEventListener('click', (element) => {
-
-    if (element.target == $btnPause) start = !start
-    if (element.target == $btnDetener) {
-      start = false;
-      clearInterval(temporizador)
-      //todo reiniciar el temporizador a 0
-      $horas.innerText = horas = 0
-      $minutos.innerText = minutos = 0
-      $segundos.innerText = segundos = 0
-    }
-
-
-
-    if (start) {
-      console.log(start, 'if')
-      temporizador = setInterval(() => {
-
-        if (segundos !== 0) {
-          segundos -= 1;
-          $segundos.innerText = segundos
-
-        } else {
-          //los segundos son 0
-
-          if (minutos !== 0) {
-
-            segundos = 59;
-            $segundos.innerText = segundos
-            minutos -= 1
-            $minutos.innerText = minutos
-
-          } else {
-            //los minutos son 0
-            if (horas !== 0) {
-              horas -= 1;
-              $horas.innerText = horas
-              minutos = 59;
-              $minutos.innerText = minutos
-            }
-          }
-        }
-
-
-
-
-        if (segundos === 0 && minutos == 0 && horas === 0) {
-          clearInterval(temporizador)
-          alert('Temporizador finalizo')
-
-        }
-
-      }, 1000)
-    } else {
-      clearInterval(temporizador)
-    }
-
-  })
-
-
-
-}
-
-// initTemporizador()
+const $audio = new Audio('/static/alarma_2.mp3')
 
 const anotherTemporizador = () => {
-  $btnDetener.disabled = true;
 
+  //! seteando las variables iniciales 
+
+  //el boton detener por defecto esta desactivado
+  $btnDetener.disabled = true;
   let start = false;
   let temporizador;
   let horas = +$horas.value
   let minutos = +$minutos.value
   let segundos = +$segundos.value
-  console.log(segundos, minutos, horas)
-  if (segundos === 0 && minutos === 0 && horas === 0) {
-    $btnPause.disabled = true;
-    $btnPause.classList.add('disable')
-  }
+
+  validateTime(horas, segundos, minutos)
   const arr = [$minutos, $horas, $segundos];
 
+  //! boton que deactiva y desaparece el popup
   $buttonPopup.addEventListener('click', function () {
+    //desactiva el popup
     $popup.classList.add('popup_disable')
+    //pausa le audio y lo resetea a 0 
+    $audio.pause();
+    $audio.currentTime = 0;
   })
 
-
+  //! funcionalidad de los botones de sumar y restar
   $buttonsFunctions.addEventListener('click', ({ target }) => {
-    // if (target == $btnHorasSumar) {
-    //   $horas.innerText = horas++
-    // }
     switch (target) {
       case $btnHorasSumar:
 
@@ -176,7 +60,6 @@ const anotherTemporizador = () => {
         $horas.value = horas
         return validateTime(segundos, minutos, horas)
       case $btnMinutosSumar:
-
 
         minutos = minutos + 1
         if (minutos == 60) {
@@ -212,6 +95,7 @@ const anotherTemporizador = () => {
     }
   })
 
+  //! funcionalidad: cada que haces un cambio en el input, se agrega el valor que tiene ese input a los valores del timer, y hace validaciones que los minutos y seg no pasen de 60 y si pasan se suma uno al correspondiente valor sucesor
 
   arr.forEach((ele, i) => ele.addEventListener('blur', function (e) {
     horas = +$horas.value
@@ -236,31 +120,39 @@ const anotherTemporizador = () => {
   }))
 
 
-
+  //! Inicia el contador 
   $buttons.addEventListener('click', (element) => {
 
     if (element.target == $btnPause) {
-
+      //cambia el valor de la variable start
       start = !start;
-      $btnDetener.classList.remove('disable')
-      $btnDetener.disabled = false;
-
     }
     if (element.target == $btnDetener) {
       start = false;
+      //limpia el temporizador
       clearInterval(temporizador)
-      //todo reiniciar el temporizador a 0
-      $horas.value = horas = 0
-      $minutos.value = minutos = 0
-      $segundos.value = segundos = 0
+      //reseteando a cero los valores 
+      horas = 0
+      minutos = 0
+      segundos = 0
+      //reseteando los valores en el HTML
+      resetValuesDOM($segundos, $minutos, $horas);
+      //desactiva el boton detener
       validateTime(segundos, minutos, horas)
+      $btnDetener.disabled = true;
       $btnDetener.classList.add('disable')
-      $btnDetener.disabled = false;
     }
 
 
 
     if (start) {
+
+      $btnDetener.disabled = false;
+      $btnDetener.classList.remove('disable')
+
+      //cambia la leyenda 
+      $btnPause.innerHTML = 'Pause'
+      //inicial el temporizador
       temporizador = setInterval(() => {
 
         if (segundos !== 0) {
@@ -288,19 +180,30 @@ const anotherTemporizador = () => {
           }
         }
 
-
-
+        validateTime(minutos, segundos, horas)
+        //cuando el temporizador finalize
         if (segundos === 0 && minutos == 0 && horas === 0) {
+          //inicia el audio
+          $audio.play()
+          //muestar el popup
           $popup.classList.remove('popup_disable')
+          //Elimina el temporizador
+          clearInterval(temporizador)
+
           validateTime(segundos, minutos, horas)
 
-          clearInterval(temporizador)
-          // alert('Temporizador finalizo')
+          $btnDetener.disabled = true;
+          $btnDetener.classList.add('disable')
+
 
         }
 
       }, 1000)
     } else {
+
+      //
+      $btnPause.innerHTML = 'play'
+      //Elimina el temporizador
       clearInterval(temporizador)
     }
 
@@ -310,8 +213,8 @@ const anotherTemporizador = () => {
 
 }
 
-
-const validateTime = (segundos, minutos, horas) => {
+//!valuda el valor de los minutos, segundos y horas, para asi activar o desactivar el boton de coninuat o pausar
+const validateTime = (segundos, minutos, horas, button) => {
   if (segundos === 0 && minutos === 0 && horas === 0) {
     $btnPause.disabled = true;
     $btnPause.classList.add('disable')
@@ -320,6 +223,13 @@ const validateTime = (segundos, minutos, horas) => {
     $btnPause.classList.remove('disable')
 
 
+  }
+}
+
+//reinicia los valores en el DOM a 0
+const resetValuesDOM = (...values) => {
+  for (const iterator of values) {
+    iterator.value = 0;
   }
 }
 
